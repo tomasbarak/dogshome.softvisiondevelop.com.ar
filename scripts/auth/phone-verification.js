ecd37298de5f963dde94c67bec1a23d67686f5fe
tomasbarak <barakcinthia@gmail.com>
2021-08-01 22:11:02 -0300

    Phone verif secure without api keys

--- scripts/auth/phone-verification.js
+++ scripts/auth/phone-verification.js
@@ -8,10 +8,7 @@ function verifyPhoneNumber(phone){
     var cleanedPhone = phone.replaceAll('-', '');
     console.log("phone = " + cleanedPhone);
     var oReq = new XMLHttpRequest();
-    var params = "To=%2B54" + phone + "&Channel=call"
     oReq.addEventListener("load", reqListener);
-    oReq.open("POST", "https://coral-newt-2178.twil.io/start?phone_number=" + phone + "&country_code=54");
-    oReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    oReq.setRequestHeader("Authorization", "Basic " + btoa("ACa3013f90e4375255cb129e2516ac46dc:e1c6c5b40480b1c8d327e13208725f8b"));
-    oReq.send(params);
+    oReq.open("POST", "https://dogshome-8339.twil.io/sendSMS?phone_number=" + phone + "&country_code=54");
+    oReq.send();
 }
