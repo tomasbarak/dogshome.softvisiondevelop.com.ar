2f32831d3bec441c9b31b0afdda0c2aee24e3440
tomasbarak <barakcinthia@gmail.com>
2021-08-02 22:46:49 -0300

    Removed credentials from phone verification

--- scripts/auth/phone-verification.js
+++ scripts/auth/phone-verification.js
@@ -13,6 +13,5 @@ function verifyPhoneNumber(phone){
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", reqListener);
     oReq.open("POST", "https://coral-newt-2178.twil.io/start?phone_number=" + cleanedPhone + "&country_code=54");
-    oReq.setRequestHeader("Authorization", "Basic " + btoa('ACa3013f90e4375255cb129e2516ac46dc' + ":" + 'e1c6c5b40480b1c8d327e13208725f8b'));
     oReq.send();
 }
