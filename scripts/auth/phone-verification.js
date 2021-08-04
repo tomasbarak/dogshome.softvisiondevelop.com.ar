992e197a154a45a08fe5c13d2d3d7011dd2e544a
tomasbarak <barakcinthia@gmail.com>
2021-08-04 17:44:03 -0300

    Added code verification method

--- scripts/auth/phone-verification.js
+++ scripts/auth/phone-verification.js
@@ -18,3 +18,25 @@ function verifyPhoneNumber(phone){
     oReq.open("POST", "https://coral-newt-2178.twil.io/start?phone_number=" + cleanedPhone + "&country_code=54");
     oReq.send();
 }
+
+function verifyCode(code){
+    var cleanedPhone = sessionStorage.getItem("sessionAccPhoneNum").replaceAll('-', '');
+    cleanedPhone = cleanedPhone.replaceAll(' ', '');
+    console.log("phone = " + cleanedPhone);
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("POST", "https://coral-newt-2178.twil.io/check?phone_number=" + cleanedPhone + "&country_code=54&verification_code=" + code);
+    oReq.send();
+}
+function getVerificationCode(){
+    var verificationCode = ""
+    var digit1 = document.getElementById('digit-1').value.toString();
+    var digit2 = document.getElementById('digit-2').value.toString();
+    var digit3 = document.getElementById('digit-3').value.toString();
+    var digit4 = document.getElementById('digit-4').value.toString();
+    var digit5 = document.getElementById('digit-5').value.toString();
+    var digit6 = document.getElementById('digit-6').value.toString();
+    verificationCode = digit1 + digit2 + digit3 + digit4 + digit5 + digit6;
+    console.log(verificationCode);
+    verifyCode(verificationCode);
+}
